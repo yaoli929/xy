b83194d4fb22cd310b88f9e2e559fdd37f8d5ad2
xy <815608528@qq.com>
2016-03-08 22:17:24 +0800

    bug修复

--- xy929/views/layouts/main.php
+++ xy929/views/layouts/main.php
@@ -58,7 +58,7 @@ AppAsset::register($this);
 
     <div class="container">
         <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : "首页",
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : "",
         ]) ?>
         <?= $content ?>
     </div>
